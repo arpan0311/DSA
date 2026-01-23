a15cc7cdc9932b481f856a641bee2fd70bbd811e
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-01-23 12:00:09 +0530

    Time: 913 ms (8.84%) | Memory: 86.4 MB (5.00%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    long long arpan(vector<int>& nums, int i, int target, int sum) {
+    
+    long long arpan(vector<int>& nums, int i, int target, int sum,map<string,int>&hash) {
         if (i == nums.size()) {
             if (sum == target) {
                 return 1;
             }
             return 0;
         }
+        // if(dp[i][sum])/
         if (i > nums.size()) {
             return 0;
         }
-        long long plus = arpan(nums, i + 1, target, sum + nums[i]);
-        long long minus = arpan(nums, i + 1, target, sum - nums[i]);
-        return plus + minus;
+       string key = to_string(i) + "_" + to_string(sum);
+        if(hash.count(key)){
+            return hash[key];
+        }
+        long long plus = arpan(nums, i + 1, target, sum + nums[i],hash);
+        long long minus = arpan(nums, i + 1, target, sum - nums[i],hash);
+      
+        hash[key]= plus + minus;
+        return hash[key];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
 
         int i = 0;
         int sum = 0;
-
-        return arpan(nums, i, target, sum);
+        map<string,int>hash;
+        return arpan(nums, i, target, sum,hash);
     }
 };
