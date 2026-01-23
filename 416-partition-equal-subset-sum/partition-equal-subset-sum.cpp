0c8c6027fab0d4fd17f4603489c4dedd561dad0b
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-01-23 10:44:45 +0530

    Time: 157 ms (29.49%) | Memory: 31 MB (47.27%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int dp[205][20005];
+    bool arpan(vector<int>&nums,int i,int curr,int sum,bool &check){
+        if(check){
+            return true;;
+        }
+        if(i>=nums.size()){
+            return false;
+        }
+        if(curr>sum){
+            return false;
+        }
+        if(dp[i][curr]!=-1){
+            return dp[i][curr];
+        }
+        if(2*curr==sum){
+            check=true;
+            return true;
+        }
+        bool take=arpan(nums,i+1,curr+nums[i],sum,check);
+        bool not_take=arpan(nums,i+1,curr,sum,check);
+        dp[i][curr]=take||not_take;
+        return dp[i][curr];
+    }
+    bool canPartition(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        long long sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        int i=0;
+        int curr=0;
+        bool check=false;
+        
+        return arpan(nums,i,curr,sum,check);
+    }
+};
