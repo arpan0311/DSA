055f977ac4905d91ad9ae4b135f3c690e190916c
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-01-24 11:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (46.16%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,35 +11,18 @@
  */
 class Solution {
 public:
-    
-    TreeNode* makeTree(vector<int>&preorder,vector<int>&inorder,unordered_map<int,int>&hash,int i,int n,int &curr){
-        int size=preorder.size();
-        if(i>n){
+    TreeNode* arpan(vector<int>&A,int &i,int maxi){
+        if(i==A.size()||A[i]>maxi){
             return NULL;
         }
-
-        if(curr>=size){
-            return NULL;
-        }
-        int value=preorder[curr++];
-       TreeNode* root= new TreeNode(value);
-        int mid=hash[value];
-        root->left=makeTree(preorder,inorder,hash,i,mid-1,curr);
-        root->right=makeTree(preorder,inorder,hash,mid+1,n,curr);
-        
+        TreeNode* root =new TreeNode(A[i++]);
+        root->left=arpan(A,i,root->val);
+        root->right=arpan(A,i,maxi);
         return root;
     }
-    TreeNode* bstFromPreorder(vector<int>& preorder) {
-        unordered_map<int,int>hash;
-       
-        vector<int>inorder(preorder.begin(),preorder.end());
-        sort(inorder.begin(),inorder.end());
-         for(int i=0;i<inorder.size();i++){
-            hash[inorder[i]]=i;
-        }
-        // now I have inorder and preorder so 
-        int i=0,n=preorder.size()-1;
-        int curr=0;
-       return makeTree(preorder,inorder,hash,i,n,curr);
+    TreeNode* bstFromPreorder(vector<int>& A) {
+        int i=0,maxi=INT_MAX;
+        // Tree* root=
+        return arpan(A,i,maxi);
     }
 };
