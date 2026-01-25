b3cfbb5afad84164ca664b290452245ae04bfc0b
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-01-25 15:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (89.17%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-int dp[1001];
-    int  arpan(vector<int>&cost,int i){
+// int dp[1001];
+//     int  arpan(vector<int>&cost,int i){
 
-        if(i>=cost.size()){
-            return 0;
-        } 
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int one=cost[i]+arpan(cost,i+1);
-        int two=cost[i]+arpan(cost,i+2);
-        dp[i]=min(one,two);
-        return dp[i];
-    }   
+//         if(i>=cost.size()){
+//             return 0;
+//         } 
+//         if(dp[i]!=-1){
+//             return dp[i];
+//         }
+//         int one=cost[i]+arpan(cost,i+1);
+//         int two=cost[i]+arpan(cost,i+2);
+//         dp[i]=min(one,two);
+//         return dp[i];
+//     }   
     int minCostClimbingStairs(vector<int>& cost) {
-        int i=0,sum=0,mini=INT_MAX;
-        memset(dp,-1,sizeof(dp));
-        int zero=arpan(cost,i);
-        int one=arpan(cost,i+1);
-        return min(zero,one);
+        int n=cost.size();
+        if(n==2){
+            return min(cost[0],cost[1]);
+        }
+        int answer=0;
+
+      for(int i=2;i<cost.size();i++){;
+      cost[i]= cost[i] + min(cost[i-1], cost[i-2]);
+
+      }
+
+      return min(cost[n-1],cost[n-2]);
     }
 };
