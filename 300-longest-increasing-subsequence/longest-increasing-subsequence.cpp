d87815c7776459f538f0fe1cb60741a40bb62abd
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-01-28 10:45:03 +0530

    Time: 4 ms (78.73%) | Memory: 14 MB (88.25%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    int dp[2501][2502];
-    int solve(vector<int>& nums, int prev, int curr) {
-        if (nums.size() == curr) {
-            return 0;
+    int binarySearch(vector<int>&temp,int target){
+        int low=0,high=temp.size()-1;
+        int value=high;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(temp[mid]>=target){
+                value=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        if (dp[curr][prev + 1] != -1) {
-            return dp[curr][prev + 1];
-        }
-        int take = 0;
-        int nottake = solve(nums, prev, curr + 1);
-        if (prev == -1 || prev !=-1 && nums[curr] > nums[prev]) {
-            take = 1 + solve(nums, curr, curr + 1);
-        }
-
-        dp[curr][prev + 1] = max(take, nottake);
-        return dp[curr][prev + 1];
+        return  value;
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int prev = -1, curr = 0;
-        memset(dp,-1,sizeof(dp));
-        return solve(nums, prev, curr);
+        int lengthOfLIS(vector<int>& nums) {
+        vector<int>temp;
+        for(int i=0;i<nums.size();i++){
+            if(temp.empty()||temp.back()<nums[i]){
+                temp.push_back(nums[i]);
+            }
+            else {
+                int ind=binarySearch(temp,nums[i]);
+                temp[ind]=nums[i];
+            }
+        }
+        return temp.size();
     }
 };
