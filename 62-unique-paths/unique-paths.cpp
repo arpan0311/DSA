68d3f19e267bb4dc4e3a27e9d7aa0c3e2cbf0269
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-01-29 19:53:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (75.71%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    int dp[101][101];
-    int arpan(vector<vector<int>>& ans, int i, int j, int& count) {
-        if (i <  0 || j < 0) {
-            return 0;
-        }
-        if (i == 0 && j == 0 ) {
-            return 1;
-        }
-        if (ans[i][j] !=-1) {
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int take=0,nottake=0;
-        if (i - 1 >=0 && ans[i - 1][j] == -1) {
-            ans[i][j] = 0;
-          take=arpan(ans, i - 1, j, count);
-           ans[i][j]=-1;
-        }
-      
-        if (j - 1 >=0  && ans[i][j - 1] == -1) {
-            ans[i][j] = 0;
-            nottake=arpan(ans, i, j - 1, count);
-            ans[i][j]=-1;
-        }
-        dp[i][j]=take+nottake;
-        return dp[i][j];
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> ans(m, vector<int>(n, -1));
-        int count = 0;
-        int i = m-1, j = n-1;
-        memset(dp,-1,sizeof(dp));
-         return arpan(ans, i, j, count);
-
-        //  tabulation kaise karu I will have to 
-
-        
+        // space optimization 
+        vector<int>prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int>curr(n);
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0){
+                    curr[0]=1;
+                }
+                else{
+                    int up=0,left=0;
+                   if(i>0){
+                        up=prev[j];
+                   }
+                   if(j>0){
+                        left=curr[j-1];
+                   }
+                   curr[j]=up+left;
+                }
+            }
+            prev=curr;
+        }
+        return prev[n-1];
     }
 };
