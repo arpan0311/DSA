3e366a0fcf353e40bed273e0eab3417e602fe329
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-02-05 17:25:57 +0530

    Time: 102 ms (21.24%) | Memory: 78.2 MB (29.97%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    static bool cmp(pair<int,int>& p1, pair<int,int>& p2) {
-        return (p1.first * p1.first + p1.second * p1.second) <
-               (p2.first * p2.first + p2.second * p2.second);
-    }
-
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+      priority_queue<
+    pair<int, vector<int>>,
+    vector<pair<int, vector<int>>>
+> pq;
+    //  priority_queue<pair<int, vector<int>>> pq;
 
-        vector<pair<int,int>> answer;
+for (auto &p : points) {
+    int dist = p[0]*p[0] + p[1]*p[1];
+    pq.push({dist, p});
 
-        for (int i = 0; i < points.size(); i++) {
-            answer.push_back({points[i][0], points[i][1]});
+    if (pq.size() > k) {
+        pq.pop();   // removes farthest point
+    }
+}
+
+        vector<vector<int>>answer;
+        while(!pq.empty()){
+            answer.push_back(pq.top().second);
+            pq.pop();
         }
-
-        sort(answer.begin(), answer.end(), cmp);
-
-        vector<vector<int>> result;
-        
-        int cnt = 0;
-
-        for (auto &it : answer) {
-            vector<int> ans;
-            if (cnt == k) break;
-            ans.push_back(it.first);
-            ans.push_back(it.second);
-            result.push_back(ans);
-            cnt++;
-        }
-
-        // result.push_back(ans);
-        return result;
+        return answer;
     }
 };
