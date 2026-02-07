5888be0ccc2b281f8e87e05f2fcf1de25a251bf7
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-02-07 17:54:03 +0530

    Time: 103 ms (69.52%) | Memory: 21.7 MB (51.89%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int dp[201][20001];
-    bool arpan(vector<int>&nums,int i,int curr,int sum,bool &check){
-        if(check){
-            return true;;
-        }
-        if(i>=nums.size()){
-            return false;
-        }
-        if(curr>sum){
-            return false;
-        }
-        if(dp[i][curr]!=-1){
-            return dp[i][curr];
-        }
-        if(2*curr==sum){
-            check=true;
+    int dp[201][10001];
+    bool arpan(vector<int>&nums,int i,int target){
+        if(target==0){
             return true;
         }
-        bool take=arpan(nums,i+1,curr+nums[i],sum,check);
-        bool not_take=arpan(nums,i+1,curr,sum,check);
-        dp[i][curr]=take||not_take;
-        return dp[i][curr];
-    }
-    bool canPartition(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        long long sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-        }
-        if(sum%2!=0){
+        if(i<0||target<0){
             return false;
         }
-        int i=0;
-        int curr=0;
-        bool check=false;
-        
-        return arpan(nums,i,curr,sum,check);
+        if(dp[i][target]!=-1){
+            return dp[i][target];
+        }
+        bool take=false;
+        if(nums[i]<=target){
+            take=arpan(nums,i-1,target-nums[i]);
+        }
+        bool not_take=arpan(nums,i-1,target);
+        return dp[i][target]= take||not_take;
+    }
+    bool canPartition(vector<int>& nums) {
+        // simple h 
+        int target=0;
+
+        for(int i=0;i<nums.size();i++){
+            target+=nums[i];
+        }
+        memset(dp,-1,sizeof(dp));
+        if(target%2!=0){
+            return false;
+        }
+        target=target/2;
+        int i=nums.size()-1;
+       return arpan(nums,i,target);
     }
 };
