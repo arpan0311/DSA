2f3e56d04aaf37b2ce144e6ffe883e2594e54666
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-02-10 19:11:55 +0530

    Time: 23 ms (71.50%) | Memory: 16 MB (70.99%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
-    int dp[301][5001];
-    int solve(vector<int>&coins,int amount,int i){
-        if(i>=coins.size()){
-            if(amount==0){
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[i][amount]!=-1){
-            return dp[i][amount];
-        }
-        int take=0;
-        if(coins[i]<=amount){
-            take=solve(coins,amount-coins[i],i);
-        }
-        int notTake=solve(coins,amount,i+1);
-        return dp[i][amount]= take+notTake;
-    }
+    // int dp[301][5001];
+    // int solve(vector<int>& coins, int amount, int i) {
+    //     if (i >= coins.size()) {
+    //         if (amount == 0) {
+    //             return 1;
+    //         }
+    //         return 0;
+    //     }
+    //     if (dp[i][amount] != -1) {
+    //         return dp[i][amount];
+    //     }
+    //     int take = 0;
+    //     if (coins[i] <= amount) {
+    //         take = solve(coins, amount - coins[i], i);
+    //     }
+    //     int notTake = solve(coins, amount, i + 1);
+    //     return dp[i][amount] = take + notTake;
+    // }
     int change(int amount, vector<int>& coins) {
-        sort(coins.begin(),coins.end());
-        memset(dp,-1,sizeof(dp));
-        return solve(coins,amount,0);
+        // sort(coins.begin(),coins.end());
+        // memset(dp,-1,sizeof(dp));
+        // return solve(coins,amount,0);
+        int dp[301][5001];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 0; i <= amount; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 0; i <= coins.size(); i++) {
+            dp[i][0] = 1;
+        }
+        int n = coins.size();
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                long long take = 0;
+                if (coins[i - 1] <= j) {
+                    // in this case I used n-1th index so I write here i not i-1
+                    take = dp[i][j - coins[i - 1]];
+                }
+                long long nottake = dp[i - 1][j];
+                dp[i][j] = take + nottake;
+            }
+        }
+        return dp[n][amount];
     }
 };
