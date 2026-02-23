f803c5d5f419f6763612d27009c6a2a5b7632b59
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-02-23 22:22:39 +0530

    Time: 13 ms (8.82%) | Memory: 24.7 MB (12.74%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int dp[30001][2];
-    int solve(vector<int>& prices, bool canbuy, int i) {
-        if (i >= prices.size()) {
-            return 0;
-        }
-        // case 1 can take
-        if (dp[i][canbuy] != -1) {
-            return dp[i][canbuy];
-        }
-        int take = 0, nottake = 0;
-        if (canbuy) {
-            take = -prices[i] + solve(prices, false, i + 1);
-            nottake = solve(prices, canbuy, i + 1);
-
-        } else {
-            take = prices[i] + solve(prices, true, i + 1);
-            nottake = solve(prices, canbuy, i + 1);
-        }
-
-        return dp[i][canbuy] = max(take, nottake);
-    }
     int maxProfit(vector<int>& prices) {
-        int curr = 0, i = 0;
-        bool cantake = true;
-        int profit = 0;
-        memset(dp, -1, sizeof(dp));
-        return solve(prices, cantake, i);
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int take = 0, notTake = 0;
+
+                if (buy) {
+                    take = -prices[i] + dp[i + 1][0];
+                    notTake = dp[i + 1][1];
+                } 
+                else {
+                    take = prices[i] + dp[i + 1][1];
+                    notTake = dp[i + 1][0];
+                }
+
+                dp[i][buy] = max(take, notTake);
+            }
+        }
+
+        return dp[0][1];
     }
 };
