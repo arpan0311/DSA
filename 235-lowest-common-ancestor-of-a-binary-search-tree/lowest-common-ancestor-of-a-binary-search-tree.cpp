cde815f9c2a408c7bcc9a83ca463ef287ef711fe
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-03-01 23:31:28 +0530

    Time: 27 ms (22.68%) | Memory: 23.6 MB (34.32%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,36 +10,18 @@
 
 class Solution {
 public:
-void path(TreeNode* root,TreeNode* p,vector<TreeNode*>&ans){
-    if(!root){
-        return ;
-    }
-    if(root->val==p->val){
-        ans.push_back(root);
-        return;
-    }
-    if(root->val<p->val){
-        ans.push_back(root);
-        path(root->right,p,ans);
-    }
-    else{
-        ans.push_back(root);
-        path(root->left,p,ans);
-    }
-}
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>pathA;
-        path(root,p,pathA);
-        vector<TreeNode*>pathB;
-        path(root,q,pathB);
-        set<TreeNode*>st;
-        for(int i=pathA.size()-1;i>=0;i--){
-            st.insert(pathA[i]);
-        }
-        for(int i=pathB.size()-1;i>=0;i--){
-            if(st.find(pathB[i])!=st.end()){
-                return pathB[i];
+        while(root){
+            if(p->val<root->val&&q->val<root->val){
+                root=root->left;
             }
+            else if(p->val>root->val&&q->val>root->val){
+                root=root->right;
+            }
+            else{
+            return root;
+            }
+            
         }
         return root;
     }
