896b47f089968182ad2b79857841448b0f8251e3
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-03-02 14:20:57 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (59.19%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -6,40 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-public:
-    void find_diff(TreeNode* root, TreeNode* child, int& maxidiff) {
-        if (!child) {
+public: 
+    void bfs(TreeNode* root,int maxi,int mini,int &max_diff){
+        if(!root){
             return;
         }
-        maxidiff = max(maxidiff, abs(root->val - child->val));
-        find_diff(root, child->left, maxidiff);
-        find_diff(root, child->right, maxidiff);
-    }
-    void bfs(TreeNode* root, int& maxidiff) {
-        // base case
-        if (!root) {
-            return;
-        }
-
-        // root left right
-
-        if (root->left) {
-             find_diff(root, root->left, maxidiff);
-        }
-        if (root->right) {
-             find_diff(root, root->right, maxidiff);
-        }
-        bfs(root->left, maxidiff);
-        bfs(root->right, maxidiff);
+        maxi=max(maxi,root->val);
+        mini=min(mini,root->val);
+        max_diff=max(max_diff,abs(maxi-mini));
+        bfs(root->left,maxi,mini,max_diff);
+        bfs(root->right,maxi,mini,max_diff);
     }
     int maxAncestorDiff(TreeNode* root) {
-        int maxidiff = 0;
-        bfs(root, maxidiff);
-        return maxidiff;
+        int maxi=root->val,mini=root->val;
+        int max_diff=abs(maxi-mini);
+        bfs(root,maxi,mini,max_diff);
+        return max_diff;
     }
 };
