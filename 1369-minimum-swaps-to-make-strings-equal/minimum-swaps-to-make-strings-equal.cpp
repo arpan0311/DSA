eee1ae34b67d3261fa1570d263e644093fc01cea
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-03-11 00:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.71%) - LeetSync

---
+++ 1369-minimum-swaps-to-make-strings-equal/minimum-swaps-to-make-strings-equal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumSwap(string s1, string s2) {
+        int xy=0,yx=0;
+        for(int i=0;i<s1.length();i++){
+            if(s1[i]=='x'&&s2[i]=='y'){
+                xy++;
+            }
+            else if(s1[i]=='y'&&s2[i]=='x'){
+                yx++;
+            }
+        }
+        if((yx+xy)%2!=0){
+            return -1;
+        }
+        int ans=xy/2+yx/2;
+        ans+=(yx%2)*2;
+        return ans;
+    }
+};
