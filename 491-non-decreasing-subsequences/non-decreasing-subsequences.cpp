f5d9cf3c455a740f9a7447fa03ba715154bb1c2c
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-03-31 14:59:05 +0530

    Time: 23 ms (78.45%) | Memory: 26.5 MB (61.82%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    void arpan(set<vector<int>> &answer, vector<int>& nums, int idx,
-               vector<int> curr) {
-        if (idx >= nums.size()) {
-            if (curr.size() >= 2) {
-                answer.insert(curr);
-            }
-            return;
-        }
+    void arpan(vector<vector<int>> &answer, vector<int>& nums, int idx,
+               vector<int>& curr) {
+
         if (curr.size() >= 2) {
-            answer.insert(curr);
+            answer.push_back(curr);
         }
 
-        // no sir
-     
+        unordered_set<int> used;
+
         for (int i = idx; i < nums.size(); i++) {
-            // valid hai can pick this element
-            if (!curr.empty() && curr.back() <= nums[i]) {
-           
-                curr.push_back(nums[i]);
-                arpan(answer, nums, i + 1, curr);
-                curr.pop_back();
-            } else if (curr.empty()) {
-                curr.push_back(nums[i]);
-                arpan(answer, nums, i + 1, curr);
-                curr.pop_back();
-            }
+
+            if (used.count(nums[i])) continue;
+
+            if (!curr.empty() && nums[i] < curr.back()) continue;
+
+            used.insert(nums[i]);
+
+            curr.push_back(nums[i]);
+            arpan(answer, nums, i + 1, curr);
+            curr.pop_back();
         }
-        return;
     }
-vector<vector<int>>
-findSubsequences(vector<int>& nums) {
-    set<vector<int>> answer;
-    vector<int> curr;
-    int i = 0;
-    arpan(answer, nums, i, curr);
-    return vector<vector<int>>(answer.begin(), answer.end());
-}
-}
-;
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<vector<int>> answer;
+        vector<int> curr;
+        arpan(answer, nums, 0, curr);
+        return answer;
+    }
+};
