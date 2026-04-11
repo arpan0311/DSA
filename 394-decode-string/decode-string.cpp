24b7244a90612e5e6d459ca3869c441874ab7fa5
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-04-11 12:38:26 +0530

    Time: 2 ms (23.25%) | Memory: 9.2 MB (78.78%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
-    string decodeString(string s) {
-        stack<char>st;
-
-        for(int i=0;i<s.length();i++){
-            if(s[i]!=']'){
-                st.push(s[i]);
-            }
-            else{
-                // decode string 
-                string curr="";
-                // got all characters
-                while(!st.empty()&&st.top()!='['){
-                    curr+=st.top();
-                    st.pop();
+    string solve(string &s, int &i) {
+        string result = "";
+        while(i < s.length() && s[i] != ']') {
+            if(isdigit(s[i])) {
+                int num = 0;
+                while(i < s.length() && isdigit(s[i])) {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
                 }
-                // [ ko hatao...
-                if(!st.empty()){
-                    st.pop();
-                }
-                reverse(curr.begin(),curr.end());
-                string num="";
-                // means digit h
-                while(!st.empty()&&isdigit(st.top())){
-                    num+=st.top();
-                    st.pop();
-                }
-                reverse(num.begin(),num.end());
-                int val=stoi(num);
-                string temp=curr;
-                curr="";
-                while(val>0){
-                    curr+=temp;
-                    val--;
-                }
-                for(auto&it:curr){
-                    st.push(it);
+                
+                i++; // skip '['
+                string temp = solve(s, i);
+                
+                i++; // skip ']'
+                while(num--) {
+                    result += temp;
                 }
             }
+            else {
+                result += s[i];
+                i++;
+            }
         }
-        string result="";
-        while(!st.empty()){
-            result+=st.top();
-            st.pop();
-        }
-        reverse(result.begin(),result.end());
+        
         return result;
+    }
+
+    string decodeString(string s) {
+        int i = 0;
+        return solve(s, i);
     }
 };
