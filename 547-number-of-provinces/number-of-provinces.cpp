6dfe782b44f781b2cb6a20fd04e8c11727252b46
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-04-16 12:49:14 +0530

    Time: 3 ms (39.03%) | Memory: 20.7 MB (29.57%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -18,7 +18,7 @@ public:
         for(int j = 0; j < n; j++) {
             if(is[i][j] == 1&&i!=j) {
                 adj[i].push_back(j);
-                adj[j].push_back(i);
+                // adj[j].push_back(i);
             }
         }
     }
