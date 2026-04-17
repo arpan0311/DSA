0064a733e044397b66d8a748feb3f488b12de9aa
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-04-17 21:59:54 +0530

    Time: 3 ms (17.84%) | Memory: 9.9 MB (8.67%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int> freq;
+        for (char c : s) freq[c]++;
+
+        stack<char> st;
+        unordered_set<char> used;
+
+        for (char c : s) {
+            freq[c]--;  // current char processed
+
+            // if already in stack → skip
+            if (used.count(c)) continue;
+
+            // maintain lexicographical order
+            while (!st.empty() && st.top() > c && freq[st.top()] > 0) {
+                used.erase(st.top());
+                st.pop();
+            }
+
+            st.push(c);
+            used.insert(c);
+        }
+
+   
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
