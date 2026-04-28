f8efd9d1596485a4cebe96786b33286f125e7687
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-04-28 22:42:16 +0530

    Time: 5 ms (44.62%) | Memory: 10.9 MB (17.46%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -7,18 +7,24 @@ public:
             hash[s1[i]]++;
         }
 
-        for(int i=0;i<s2.length();i++){
-            if(hash.find(s2[i])==hash.end()){
-                continue;
-            }
-            unordered_map<char,int>hash1;
-            for(int j=i;j<i+s1.length()&&j<s2.length();j++){
-                hash1[s2[j]]++;
+        unordered_map<char,int>hash1;
+        int i=0;
+        int j=0;
+        while(i<s2.size()){
+            hash1[s2[i]]++;
+            while(i-j+1>s1.length()){
+              hash1[s2[j]]--;
+              if(hash1[s2[j]]==0){
+                hash1.erase(s2[j]);
+              }
+              j++;
             }
             if(hash1==hash){
                 return true;
             }
+            i++;
         }
-            return false;
+
+        return false;
     }
 };
