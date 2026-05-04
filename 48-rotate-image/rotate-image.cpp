39475f114eb5500eb3e5a4f1cb0248d699fed1e7
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-05-04 14:41:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (92.70%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>>ans;
-        // traverse collumn wise 
+        
         for(int i=0;i<matrix.size();i++){
-            vector<int>temp;
             for(int j=0;j<matrix[0].size();j++){
-                temp.push_back(matrix[j][i]);
+                if(i<j){
+            swap(matrix[i][j],matrix[j][i]);
+                } 
             }
-            reverse(temp.begin(),temp.end());
-            ans.push_back(temp);
         }
-        matrix=ans;
+        for(auto&it:matrix){
+            reverse(it.begin(),it.end());
+        }
+
+
     }
 };
