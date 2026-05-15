909a939cc8328266e91981d3dfe3b4d75c44c4e4
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-05-15 16:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (57.25%) - LeetSync

--- 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.cpp
+++ 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> concatWithReverse(vector<int>& nums) {
-        vector<int>ans(nums.begin(),nums.end());
+        vector<int>answer(nums.begin(),nums.end());
         reverse(nums.begin(),nums.end());
+        // 3 2 1 
         for(int i=0;i<nums.size();i++){
-            ans.push_back(nums[i]);
+            answer.push_back(nums[i]);
+
         }
-        return ans;
+        return answer;
     }
 };
