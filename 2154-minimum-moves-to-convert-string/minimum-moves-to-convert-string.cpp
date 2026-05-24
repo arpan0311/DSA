1cc820a1094c3ed977f0834dbf620b4eb76b6d2f
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-05-24 15:16:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (31.40%) - LeetSync

--- 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.cpp
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int minimumMoves(string s) {
-       int cnt =0;
-       for(int i=0;i<s.length();i++){
+        int cnt=0;
+        for(int i=0;i<s.length();i++){
             if(s[i]=='O'){
                 continue;
             }
             if(s[i]=='X'){
-                i=i+2;
                 cnt++;
+                i=i+2;
             }
-       }
-       return cnt;
+        }
+        return cnt;
     }
 };
