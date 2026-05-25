35dffc55e6f0e4be16ccfa75bfc99809c6d5a1b4
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-05-25 20:25:29 +0530

    Time: 4 ms (33.90%) | Memory: 13 MB (16.38%) - LeetSync

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.cpp
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int minimumBuckets(string s) {
-        int cnt = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] != 'H') {
-                continue;
-            }
-            if (i - 1 >= 0 && s[i - 1] == 'V') {
+        int cnt=0;
+        for(int i=0;i<s.length();i++){
+            // . V
+            if(s[i]!='H'){
                 continue;
             }
 
-            else if (i + 1 < s.length() && s[i + 1] == '.') {
-                s[i + 1] = 'V';
+            if(i-1>=0&&s[i-1]=='V'){
+                continue;
+            }
+
+            if(i+1<=s.length()&&s[i+1]=='.'){
+                s[i+1]='V';
                 cnt++;
-            } else if (i - 1 >= 0 && s[i - 1] == '.') {
-                s[i - 1] = 'V';
+            }
+            else if(i-1>=0&&s[i-1]=='.'){
+                s[i-1]='V';
                 cnt++;
-            } else {
+            }
+            else{
                 return -1;
             }
         }
