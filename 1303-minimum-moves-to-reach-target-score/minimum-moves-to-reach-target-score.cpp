04465d6f9fe00c4d1fbc5a2ad6f512bd77aefc13
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-05-26 14:07:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (63.54%) - LeetSync

---
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        int cnt=0;
+        while(target>0&&maxDoubles>0){
+            if(target%2==0){
+                target=target/2;
+                maxDoubles--;
+            }
+            else{
+                target--;
+            }
+            cnt++;
+        }
+        return cnt+target-1;
+    }
+};
