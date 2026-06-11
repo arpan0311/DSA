4f786fd5a6c34685af422c7da3940084a9542c8a
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-06-11 22:04:24 +0530

    Time: 31 ms (8.71%) | Memory: 22 MB (5.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    void solve(int i,int j,vector<vector<int>>&mat){
-        for(int k=0;k<mat.size();k++){
-            mat[k][j]=0;
-        }
-        for(int k=0;k<mat[0].size();k++){
-            mat[i][k]=0;
-        }
-    }
-    void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>>answer(matrix.begin(),matrix.end());
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==0){
-                    solve(i,j,answer);
-                }
+    void solve(vector<pair<int, int>>& answer, vector<vector<int>>& mat) {
+        for (auto& it : answer) {
+            int i = it.first;
+            int j = it.second;
+            for (int k = 0; k < mat.size(); k++) {
+                mat[k][j] = 0;
+            }
+            for (int k = 0; k < mat[0].size(); k++) {
+                mat[i][k] = 0;
             }
         }
-        matrix=answer;
     }
-};
+        void setZeroes(vector<vector<int>> & matrix) {
+            vector<pair<int, int>> container;
+            for (int i = 0; i < matrix.size(); i++) {
+                for (int j = 0; j < matrix[0].size(); j++) {
+                    if (matrix[i][j] == 0) {
+                        container.push_back({i, j});
+                    }
+                }
+            }
+            solve(container, matrix);
+        }
+    };
