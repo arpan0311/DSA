545108b430b9df3dcedcc12abfd5f51da8d8ddbb
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-06-16 14:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (53.72%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -8,34 +8,47 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
+ListNode* reverseList(ListNode* head) {
+        if(head==nullptr||head->next==nullptr){
+            return head;
+        }
+        ListNode* prev=nullptr;
+        ListNode* curr=head;
+        ListNode* next1=head->next;
+        while(next1!=nullptr){
+            curr->next=prev;
+            prev=curr;
+            curr=next1;
+            next1=next1->next;
+        }
+        curr->next=prev;
+        return curr;
+    }
     void reorderList(ListNode* head) {
-        ListNode* temp=head;
-        vector<int>answer;
-        while(temp!=NULL){
-            answer.push_back(temp->val);
-            temp=temp->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr&&fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int i=1,j=answer.size()-1;
-        bool left=true;
-        ListNode* temp1=head;
-        temp1->next=NULL;
-        ListNode* dummy=temp1;
-        while(i<=j){
-            if(left){
-                temp1->next= new ListNode(answer[j]);
-                j--;
-                left=false;
-                temp1=temp1->next;
-            }
-            else{
-                temp1->next=new ListNode(answer[i]);
-                i++;
-                left=true;
-                temp1=temp1->next;
-            }
+        ListNode* rev=reverseList(slow);
+
+        ListNode* curr=head;
+        while(rev->next!=nullptr){
+            ListNode* currNext=curr->next;
+            curr->next=rev;
+
+            ListNode* revNext=rev->next;
+            rev->next=currNext;
+
+            curr=currNext;
+            rev=revNext;
+
         }
-        head=dummy;
+
+
     }
 };
