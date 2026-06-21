93ab397fc83ddf0c17d62b0a6acc1ca26eed92ea
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-06-21 15:03:00 +0530

    Time: 84 ms (23.42%) | Memory: 104.9 MB (98.78%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-static bool cmp(pair<int,int>&a,pair<int,int>&b){
-    if(a.first==b.first){
-        return a.second>b.second;
-    }
-    return a.first<b.first;
-}
     int maximumBeauty(vector<int>& nums, int k) {
-        vector<pair<int,int>>answer;
+        // using binary search 
+        int res=0;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
-            int left=max(0,nums[i]-k),right=nums[i]+k;
-            answer.push_back({left,1});
-            answer.push_back({right,-1});
+            int find=nums[i]+2*k;
+            int low=i+1,high=n-1;
+            int ans=i;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                if(nums[mid]>find){
+                    high=mid-1;
+                }
+                else if(nums[mid]<find){
+                    // save and move;
+                    ans=mid;
+                    low=mid+1;
+                }
+                else{
+                    // save and move
+                    ans=mid;
+                    low=mid+1;
+                }
+            }
+            res=max(res,ans-i+1);
         }
-        sort(answer.begin(),answer.end(),cmp);
-
-        int maxi=0,curr=0;
-        for(int i=0;i<answer.size();i++){
-            curr+=answer[i].second;
-             maxi=max(maxi,curr);
-        }
-        return maxi;
+        return res;
     }
 };
