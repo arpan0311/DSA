7445f6f442f320f00c9ef5e19798d3331c056d58
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-06-22 13:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (41.09%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>answer(m+n,0);
-
-        int i=0,j=0,idx=0;
-        while(i<m&&j<n){
-            if(nums1[i]<nums2[j]){
-                answer[idx]=nums1[i];
-                i++;
-            }
-            else{
-                answer[idx]=nums2[j];
-                j++;
-            }
-            idx++;
-        }
-
-        while(i<m){
-            answer[idx]=nums1[i];
-            i++;
-            idx++;
-        }
-
+        int idx= m + n - 1;
+        int i = m - 1;    
+        int j = n - 1; 
         
-        while(j<n){
-            answer[idx]=nums2[j];
-            j++;
-            idx++;
+        while (j >= 0) { 
+            if (i >= 0 && nums1[i] > nums2[j]) { 
+                nums1[idx] = nums1[i];
+                i--;
+            } else {
+                nums1[idx] = nums2[j];
+                j--;
+            }
+            idx--; 
         }
-        nums1=answer;
     }
 };
