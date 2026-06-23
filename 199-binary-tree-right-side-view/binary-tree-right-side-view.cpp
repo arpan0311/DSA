744054423cf34665adca35d3885da97b0484013c
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-06-23 17:16:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (41.35%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,29 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> answer;
-        if (!root) return answer;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
-
-                // last node of this level
-                if (i == size - 1) {
-                    answer.push_back(temp->val);
+                if (root == NULL) {
+                    return {};
                 }
+                queue<TreeNode*> q;
+                q.push(root);
 
-                if (temp->left) q.push(temp->left);
-                if (temp->right) q.push(temp->right);
+                vector<int> answer;
+                while (!q.empty()) {
+                    int size = q.size();
+                    vector<int> temp;
+                    for (int i = 0; i < size; i++) {
+                        TreeNode* curr = q.front();
+                        q.pop();
+                        if (curr->left != NULL) {
+                            q.push(curr->left);
+                        }
+                        if (curr->right != NULL) {
+                            q.push(curr->right);
+                        }
+                        temp.push_back(curr->val);
+                    }
+                    answer.push_back(temp.back());
+                }
+                return answer;
             }
-        }
-
-        return answer;
-    }
-};
+        };
