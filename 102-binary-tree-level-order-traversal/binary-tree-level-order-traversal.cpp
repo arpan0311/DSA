a648f3c50a720e1b3f53d212a340155c49818d38
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-06-23 16:16:41 +0530

    Time: 3 ms (26.29%) | Memory: 16.9 MB (90.00%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,8 +6,7 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -16,25 +15,27 @@ public:
         if(root==NULL){
             return {};
         }
-        vector<vector<int>> ans;
-        queue<TreeNode*> q;
+        queue<TreeNode*>q;
         q.push(root);
-        while (!q.empty()) {
-            int n = q.size();
-            vector<int> answer;
-            for (int i = 0; i < n; i++) {
-                TreeNode* node = q.front();
-                if (node->left != nullptr) {
-                    q.push(node->left);
-                }
-                if (node->right != nullptr) {
-                    q.push(node->right);
-                }
-                answer.push_back(node->val);
+
+        vector<vector<int>>answer;
+
+        while(!q.empty()){
+            int size=q.size();
+            vector<int>temp;
+            for(int i=0;i<size;i++){
+                TreeNode* curr=q.front();
                 q.pop();
+                if(curr->left!=NULL){
+                    q.push(curr->left);
+                }
+                if(curr->right!=NULL){
+                    q.push(curr->right);
+                }
+                temp.push_back(curr->val);
             }
-            ans.push_back(answer);
+            answer.push_back(temp);
         }
-        return ans;
+        return answer;
     }
 };
