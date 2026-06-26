b050bb217413c02323c417a849add94cc01e0e2c
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-06-27 04:59:13 +0530

    Time: 7 ms (59.69%) | Memory: 20.8 MB (35.65%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,51 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
-    void solve(TreeNode* root, int targetSum, unordered_map<long long, int>& hash,
-               int& cnt, long long sum) {
-       
-        if (!root) {
+    void dfs(TreeNode* root, unordered_map<long long, int>& hash,
+             int& cnt, long long sum, int targetSum) {
+
+        if (root == nullptr) {
             return;
         }
-          if (root) {
-            sum += root->val;
-        }
+
+        sum += root->val;
+
         if (hash.find(sum - targetSum) != hash.end()) {
             cnt += hash[sum - targetSum];
         }
-       
-        
+
         hash[sum]++;
 
-        if (root->left) {
-            solve(root->left,targetSum,hash,cnt,sum);
-        }
-        if(root->right){
-             solve(root->right,targetSum,hash,cnt,sum);
-        }
+        dfs(root->left, hash, cnt, sum, targetSum);
+        dfs(root->right, hash, cnt, sum, targetSum);
+
         hash[sum]--;
     }
+
     int pathSum(TreeNode* root, int targetSum) {
-        // dfs with unordered_map
-        if(!root){
-            return 0;
-        }
         unordered_map<long long, int> hash;
         hash[0] = 1;
+
         int cnt = 0;
         long long sum = 0;
-        solve(root, targetSum, hash, cnt, sum);
+
+        dfs(root, hash, cnt, sum, targetSum);
+
         return cnt;
     }
 };
