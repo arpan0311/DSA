1f9f2a0f1d0b98ad506613c7b24b80eb5cbbfb6f
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-06-30 05:21:21 +0530

    Time: 3 ms (60.12%) | Memory: 13.9 MB (95.03%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
+    int solve(vector<int>&nums,int d){
+        // I need no pairs that has minimum or equal to distance d ye hi hain na...
+        int i=0,j=1;
+        int pair=0;
+        while(j<nums.size()){
+            int dis=nums[j]-nums[i];
+            while(nums[j]-nums[i]>d){
+                i++;
+            }
+
+            pair+=(j-i);// no of pairs that we need to add 
+            j++;
+        }
+        return pair;
+    }
     int smallestDistancePair(vector<int>& nums, int k) {
-        vector<int>answer;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-                int diff=abs(nums[i]-nums[j]);
-                answer.push_back(diff);
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int low=0,high=nums[n-1]-nums[0];
+        int answer=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int pair=solve(nums,mid);
+            if(pair<k){
+                low=mid+1;
+            }
+            else{
+                answer=mid;// store answer
+                high=mid-1; 
             }
         }
-        
-    
-        nth_element(answer.begin(),answer.begin()+(k-1),answer.end());
-        return answer[k-1];
+        // answer to distance hai naa 
+        return answer;
     }
 };
