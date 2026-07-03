64ba2c0a1c5b27d170f37610b0f30461527db62c
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-07-04 00:14:23 +0530

    Time: 70 ms (5.06%) | Memory: 26.2 MB (5.05%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // zero ki swaping hogi nonzero element ke sath
-        int i = 0;
-        int n = nums.size();
-        while (i < nums.size()) {
-            if (nums[i] == 0) {
-                int j = i + 1;
-                while (j < n && nums[j] == 0) {
-                    j++;
-                }
-                if (j < n) {
-                    swap(nums[i], nums[j]);
-                }
+        vector<pair<int,int>>answer; //value,index
+
+            for(int i=0;i<nums.size();i++){
+                answer.push_back({nums[i],i});
             }
-            i++;
-        }
+            int i=0;
+            while(i<nums.size()){
+                if(nums[i]!=0){
+                    i++;
+                    continue;
+                }
+                if(nums[i]==0){
+                    // we have to find num[j] it doesn't 
+                    int j=i+1;
+                    while(j!=nums.size()&&nums[j]==0){
+                        j++;
+                    }
+                    if(j<nums.size()){
+                        swap(nums[i],nums[j]);
+                    }
+                }
+
+               i++;
+            }
+
     }
 };
