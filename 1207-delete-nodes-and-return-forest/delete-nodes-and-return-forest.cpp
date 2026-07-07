3cb6dba536be5007eed5dabc82d2efec06b0362b
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-07-07 16:23:25 +0530

    Time: 510 ms (5.05%) | Memory: 195.4 MB (5.05%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -1,27 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* answer(TreeNode* root, unordered_set<int>& st,
-                     vector<TreeNode*>& result) {
-        if (!root) return NULL;
-        root->left = answer(root->left, st, result);
-        root->right = answer(root->right, st, result);
-        if (st.find(root->val) != st.end()) {
-            if (root->left) result.push_back(root->left);
-            if (root->right) result.push_back(root->right);
+    TreeNode* dfs(TreeNode* root,unordered_set<int>st,vector<TreeNode*>&res){
+        if(!root){
             return NULL;
         }
-        return root;
-    }
+        root->left=dfs(root->left,st,res);
+        root->right=dfs(root->right,st,res);
+        if(st.find(root->val)!=st.end()){
+            if(root->left){
+                res.push_back(root->left);
+            }
 
-    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        unordered_set<int> st(to_delete.begin(), to_delete.end());
-        vector<TreeNode*> result;
-        root = answer(root, st, result);
-        // means abhi tk delete nhi hua...
-        if (root != NULL) {
-            result.push_back(root);
+            if(root->right){
+                res.push_back(root->right);
+            }
+            return NULL;
         }
 
-        return result;
+        return root;
     }
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& nums) {
+        if(!root){
+            return {};
+        }
+        unordered_set<int>st;
+        vector<TreeNode*>result;
+
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+
+         root =dfs(root,st,result);
+
+        if(root&&st.find(root->val)==st.end()){
+            result.push_back(root);
+        }
+        return result;
+    }   
 };
