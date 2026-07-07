075d9caba660e459e1ca427bb85887cf0aba35d0
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-07-07 14:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (61.00%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -6,23 +6,29 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
- void swap(TreeNode* root){
-    if(!root){
-        return;
-    }
-    swap(root->left);
-    swap(root->right);
-    TreeNode* temp=root->left;
-    root->left=root->right;
-    root->right=temp;
- }
 class Solution {
 public:
+    void dfs(TreeNode* root) {
+        if (!root) {
+            return;
+        }
+        // post order 
+        dfs(root->left);
+        dfs(root->right);
+        TreeNode* root_left = root->left;
+        root->left = root->right;
+        root->right = root_left;
+        
+    }
     TreeNode* invertTree(TreeNode* root) {
-                swap(root);
-                return root;
+        if (!root) {
+            return root;
+        }
+        dfs(root);
+        return root;
     }
 };
