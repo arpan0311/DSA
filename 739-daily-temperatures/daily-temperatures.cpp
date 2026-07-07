53695d751a89040140c75fad8082d74465c85b51
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-07-07 23:58:41 +0530

    Time: 47 ms (5.19%) | Memory: 102.8 MB (91.64%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -3,21 +3,19 @@ public:
     vector<int> dailyTemperatures(vector<int>& nums) {
         stack<pair<int,int>>st;
         vector<int>answer(nums.size(),0);
-        int n=nums.size();
-        for(int i=n-1;i>=0;i--){
+        for(int i=nums.size()-1;i>=0;i--){
             if(st.empty()){
                 st.push({nums[i],i});
             }
-            else{
+            else {
                 
-                while(!st.empty()&&nums[i]>=st.top().first){
+                while(!st.empty()&&st.top().first<=nums[i]){
                     st.pop();
                 }
-                
-                if(!st.empty()){
+                 if(!st.empty()&&nums[i]<st.top().first){
                     answer[i]=st.top().second-i;
                 }
-                st.push({nums[i],i});
+                st.push({nums[i],i});  
             }
         }
         return answer;
