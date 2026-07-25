29442d33c7e56a958c47338efa2c0a45e1ac5939
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-07-26 03:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (97.70%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>hash;
+
         for(int i=0;i<nums.size();i++){
             hash[nums[i]]++;
         }
-        vector<int>answer;
-        // top k yani max to min heap leni padegi na...
-        priority_queue<int,vector<int>,greater<int>>pq;
+       priority_queue<pair<int,int> ,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         for(auto&it:hash){
-            pq.push(it.second);
+            pq.push({it.second,it.first});
             while(pq.size()>k){
                 pq.pop();
             }
         }
-        unordered_map<int,int>frec;
+        vector<int>answer;
         while(!pq.empty()){
-            frec[pq.top()]++;
+            answer.push_back(pq.top().second);
             pq.pop();
         }
-        for(auto&it:hash){
-            if(frec.find(it.second)!=frec.end()){
-                answer.push_back(it.first);
-            }
-        }
-        return answer;
+
+       return answer;
     }
 };
