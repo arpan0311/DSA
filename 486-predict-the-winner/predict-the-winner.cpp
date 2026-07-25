a81cd9bcabf41a0c6bae48c1e77e9ca95b5ecf2d
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-07-26 00:05:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (70.14%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+int dp[23][23];
     int solve(int i,int j,vector<int>&nums){
         // base condition 
         if(i>j){
@@ -8,14 +9,18 @@ public:
         if(i==j){
             return nums[i];
         }
-
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        // tune apna best le ;iya ab 2nd wala player jaan laga dega terko minimize karegaaaaa 
         int take_i=nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums));
         int take_j=nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums));
-        return max(take_i,take_j);
+        return dp[i][j]=max(take_i,take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
      int total_0 = accumulate(nums.begin(), nums.end(), 0);
+     memset(dp,-1,sizeof(dp));
         int first= solve(0,n-1,nums);
         return first>=total_0-first;
     }
