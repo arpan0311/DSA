99d0ffc885dcee5a1485613c6d9a37d976b848cf
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-07-30 00:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (51.48%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
+
 public:
-int dp[10000];
-    int solve(int i, vector<int>& nums) {
-        if (i >= nums.size() - 1) {
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int maxi = INT_MAX;
-        for (int j = 1; j <= nums[i]; j++) {
-            if (solve(i + j, nums) != INT_MAX) {
-                maxi = min(maxi, 1 + solve(i + j, nums));
-            }
-        }
-        return dp[i]=maxi;
-    }
     int jump(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-         return solve(0, nums); }
+        int l=0;
+        int r=0;
+        int jumps=0;
+        while(r<nums.size()-1){
+            int fouthermost=0;
+            for(int i=l;i<=r;i++){
+                fouthermost=max(i+nums[i],fouthermost);
+            }
+            jumps++;
+            l=r+1;
+            r=fouthermost;
+        }
+        
+        return jumps;
+    }
 };
