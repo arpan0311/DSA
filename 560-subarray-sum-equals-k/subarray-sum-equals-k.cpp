61fdb1e95e23024873df08dde0d1f31dd27cc9c3
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-07-31 17:07:34 +0530

    Time: 36 ms (81.44%) | Memory: 45.5 MB (56.23%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        long long sum=0;
-        int i=0,j=0;
-        // x-k element aara 
-        unordered_map<long long,int>hash;// value and index
-        long long answer=0;
+        int cnt=0;
+        unordered_map<int,int>hash;
         hash[0]=1;
-        while(i<nums.size()){
-            sum+=nums[i];
-            if(hash.find(sum-k)!=hash.end()){
-                answer+=hash[sum-k];
-            }
-            hash[sum]++;
-            i++;
+        int i=0,j=0;
+        int curr_sum=0;
+        while(j<nums.size()){
+            curr_sum+=nums[j];
+            if(hash.find(curr_sum-k)!=hash.end()){
+                cnt+=hash[curr_sum-k];
+            } 
+            hash[curr_sum]++;
+            j++;
         }
-        return answer;
+        return cnt;
     }
 };
