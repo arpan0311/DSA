8d8ea20a312a20ee6b7d4a122db7038cdfe93f64
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-01 22:31:04 +0530

    Time: 3 ms (67.80%) | Memory: 12.2 MB (50.59%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+
+    Node* dfs(Node* node) {
+        if (mp.count(node))
+            return mp[node];
+
+        Node* clone = new Node(node->val);
+        mp[node] = clone;
+
+        for (Node* nei : node->neighbors) {
+            clone->neighbors.push_back(dfs(nei));
+        }
+
+        return clone;
+    }
+
+    Node* cloneGraph(Node* node) {
+        if (!node)
+            return nullptr;
+
+        return dfs(node);
+    }
+};
