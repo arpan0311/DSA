6a1bb67c606644772cb10cf83fb5c6efc5fddca9
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-03 22:35:32 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (93.12%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>& graph, int s, int bit, vector<int>& vis) {
+        vis[s] = bit;
 
-    bool dfs(int curr,vector<vector<int>>& adj, vector<int>&vis,int colour){
-        vis[curr]=colour;
-        for(auto&it:adj[curr]){
-            if(vis[it]==-1){
-                // same colour not bipartite
-                if(!dfs(it,adj,vis,colour^1)){
+        for (auto& it : graph[s]) {
+            // not visited
+            if (vis[it] == -1) {
+                if (!dfs(graph, it, bit ^ 1, vis)) {
                     return false;
                 }
-            }
-            // same colour not bipartite
-            else if(colour==vis[it]){
-                return false;
+            } else {
+                if (vis[it] == bit) {
+                    return false;
+                }
             }
         }
         return true;
     }
-    bool isBipartite(vector<vector<int>>& adj) {
-        int n=adj.size();
-        vector<int>vis(n,-1);
-        for(int i=0;i<n;i++){
-            if(vis[i]==-1){
-                // odd length cycle h 
-                if(!dfs(i,adj,vis,0)){
+    bool isBipartite(vector<vector<int>>& graph) {
+        int bit = 1;
+        int n = graph.size();
+        vector<int> vis(n, -1);
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == -1) {
+                if (!dfs(graph, i, bit, vis)) {
                     return false;
                 }
             }
         }
-    // no odd length cycle 
         return true;
     }
 };
