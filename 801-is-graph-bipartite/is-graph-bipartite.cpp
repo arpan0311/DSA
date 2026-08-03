beab44b7f1d22ba56a1f24fc106728f0facf9b5e
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-04 00:42:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (14.04%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,20 +1,47 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, int s, int bit, vector<int>& vis) {
+    // bool dfs(vector<vector<int>>& graph, int s, int bit, vector<int>& vis) {
+    //     vis[s] = bit;
+
+    //     for (auto& it : graph[s]) {
+    //         // not visited
+    //         if (vis[it] == -1) {
+    //             if (!dfs(graph, it, bit ^ 1, vis)) {
+    //                 return false;
+    //             }
+    //         } else {
+    //             if (vis[it] == bit) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool bfs(vector<vector<int>>& graph, int s, int bit, vector<int>& vis) {
+
+        queue<int> q;
+        q.push(s);
         vis[s] = bit;
 
-        for (auto& it : graph[s]) {
-            // not visited
-            if (vis[it] == -1) {
-                if (!dfs(graph, it, bit ^ 1, vis)) {
-                    return false;
-                }
-            } else {
-                if (vis[it] == bit) {
+        while (!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            for (int nei : graph[node]) {
+
+                if (vis[nei] == -1) {
+
+                    vis[nei] = vis[node] ^ 1;
+                    q.push(nei);
+
+                } else if (vis[nei] == vis[node]) {
+
                     return false;
                 }
             }
         }
+
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
@@ -23,7 +50,7 @@ public:
         vector<int> vis(n, -1);
         for (int i = 0; i < n; i++) {
             if (vis[i] == -1) {
-                if (!dfs(graph, i, bit, vis)) {
+                if (!bfs(graph, i, bit, vis)) {
                     return false;
                 }
             }
