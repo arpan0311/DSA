4ccd8b596f9ccc83d8b7b391d79cf904d89954d3
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-05 01:05:04 +0530

    Time: 59 ms (90.93%) | Memory: 21.9 MB (51.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,61 +1,31 @@
 class Solution {
 public:
-    // int dp[201][10001];
-    // bool arpan(vector<int>&nums,int i,int target){
-    //     if(target==0){
-    //         return true;
-    //     }
-    //     if(i<0||target<0){
-    //         return false;
-    //     }
-    //     if(dp[i][target]!=-1){
-    //         return dp[i][target];
-    //     }
-    //     bool take=false;
-    //     if(nums[i]<=target){
-    //         take=arpan(nums,i-1,target-nums[i]);
-    //     }
-    //     bool not_take=arpan(nums,i-1,target);
-    //     return dp[i][target]= take||not_take;
-    // }
-    bool canPartition(vector<int>& nums) {
-        // simple h 
-        // int target=0;
-
-        // for(int i=0;i<nums.size();i++){
-        //     target+=nums[i];
-        // }
-        // memset(dp,-1,sizeof(dp));
-        // if(target%2!=0){
-        //     return false;
-        // }
-        // tabulation code 
-        int target=0;
-        for(int i=0;i<nums.size();i++){
-            target+=nums[i];
-        }
-        if(target%2!=0){
+    int dp[201][10001];
+    bool solve(int curr, int i, int total_sum, vector<int>& nums) {
+        if (i == nums.size()) {
+            if (curr == total_sum / 2) {
+                return true;
+            }
             return false;
         }
-        target=target/2;
-        vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,-1));
-        for(int i=0;i<=nums.size();i++){
-            dp[i][0]=1;//true
+        if (curr == total_sum / 2) {
+            return true;
+        } else if (curr > total_sum / 2) {
+            return false;
         }
-        for(int i=1;i<=target;i++){
-            dp[0][i]=0;//false
+        if (dp[i][curr] != -1) {
+            return dp[i][curr];
         }
-        for(int i=1;i<=nums.size();i++){
-            for(int j=1;j<=target;j++){
-                int take=0;
-                if(nums[i-1]<=j){
-                    take=dp[i-1][j-nums[i-1]];
-                }
-                int not_take=dp[i-1][j];
-                dp[i][j]=take||not_take;
-            }
+
+        return dp[i][curr] = solve(curr + nums[i], i + 1, total_sum, nums) ||
+                             solve(curr, i + 1, total_sum, nums);
+    }
+    bool canPartition(vector<int>& nums) {
+        int total_sum = accumulate(nums.begin(), nums.end(), 0);
+        if (total_sum % 2 != 0) {
+            return false;
         }
-    //    return arpan(nums,i,target);
-    return dp[nums.size()][target];
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, total_sum, nums);
     }
 };
