93299936721e013dbcf747679b329fb794958d26
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-12 17:17:52 +0530

    Time: 263 ms (5.00%) | Memory: 14.6 MB (95.76%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,79 +1,47 @@
 class Solution {
 public:
-    // ind target
-    // int dp[13][10001];
-    // int solve(vector<int>& nums, int i, int target) {
-    //     // already le liya
-    //     if (target == 0) {
-    //         return 0;
-    //     }
-    //     if (i < 0) {
-    //         return INT_MAX-target;
-    //     }
-    //     if(dp[i][target]!=-1){
-    //         return dp[i][target];
-    //     }
-    //     int not_take =  solve(nums, i - 1, target);
-    //     int take = INT_MAX;
-    //     if (target - nums[i] >= 0) {
-    //         int get=solve(nums,i,target-nums[i]);
-    //        if(get!=INT_MAX){
-    //             take=1+get;
-    //        }
-    //     }
-        
-    //     dp[i][target]= min(take, not_take);
-    //     return dp[i][target];
-    // }
-    // int coinChange(vector<int>& coins, int amount) {
-        // if(amount==0){
-        //     return 0;
-        // }
-        // sort(coins.begin(),coins.end());
-        // memset(dp,-1,sizeof(dp));
-        // int ans=solve(coins, coins.size()-1, amount);
-        // if(ans>10000){
-        //     return -1;
-        // }
-        // return ans;
+    long long dp[13][10001];
 
-//         // top down 
-//         int dp[coins.size()+1][amount+1];
-//         dp[0][0]=1;
-//         // base condition 
-//         for(int i=0;i<=target;i++){
-//             dp[0][target]=1;
-//         }
-//         for(int i=0;i<coins.size();i++){
-//             for(int j=1;j<=amount;j++){
-//                 int take=INT_MAX,
-//                 take=1+dp[i][j-1];
-//                 int nottake=dp[i][j-1];
-//                 dp[i][j]=max(take,nottake);
-//             }
-//         }
-//         return dp[coins.size()][amount];
-//     }
-// };
-int coinChange(vector<int>& coins, int amount) {
-    int n = coins.size();
-    vector<vector<int>> dp(n+1, vector<int>(amount+1, 1e9));
+    long long solve(vector<int>& nums, long long sum, int m, long long amount) {
 
-    for(int i=0;i<=n;i++){
-        dp[i][0] = 0;
-    }
-
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=amount;j++){
-            int nottake = dp[i-1][j];
-            int take = 1e9;
-            if(j >= coins[i-1]){
-                take = 1 + dp[i][j - coins[i-1]];
-            }
-            dp[i][j] = min(take, nottake);
+        if (sum > amount) {
+            return LLONG_MAX;
         }
+
+        if (sum == amount) {
+            return 0;
+        }
+
+        if (dp[m][sum] != -1) {
+            return dp[m][sum];
+        }
+
+        long long answer = LLONG_MAX;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] + sum <= amount) {
+
+                long long get_val =
+                    solve(nums, sum + nums[i], i + 1, amount);
+
+                if (get_val != LLONG_MAX) {
+                    answer = min(answer, 1 + get_val);
+                }
+            }
+        }
+
+        return dp[m][sum] = answer;
     }
 
-    return dp[n][amount] >= 1e9 ? -1 : dp[n][amount];
-}
+    int coinChange(vector<int>& coins, int amount) {
+
+        sort(coins.begin(), coins.end());
+
+        memset(dp, -1, sizeof(dp));
+
+        long long answer = solve(coins, 0, 0, amount);
+
+        return (answer == LLONG_MAX ? -1 : answer);
+    }
 };
