c6f2943226b52bd1a4a475e5f58fa72b44861990
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-15 00:03:15 +0530

    Time: 19 ms (13.50%) | Memory: 11.9 MB (16.88%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-bool solve(unordered_map<char,int>&hash){
-    bool can=false;
-    for(auto&it:hash){
-        if(it.second<=2){
-            continue;
-        }
-        else if(it.second>2){
-            return false;
-        }
-    }
-    return true;
-}
     int maximumLengthSubstring(string s) {
-        int len=0;
-        for(int i=0;i<s.length();i++){
-            unordered_map<char,int>mp;
-            for(int j=i;j<s.length();j++){
-                mp[s[j]]++;
-                if(solve(mp)){
-                    len=max(len,j-i+1);
+        int len = 0;
+        for (int i = 0; i < s.length(); i++) {
+            vector<int> frec(26, 0);
+            for (int j = i; j < s.length(); j++) {
+                frec[s[j] - 'a']++;
+                int maxi = *max_element(frec.begin(), frec.end());
+                if (maxi <= 2) {
+                    len = max(len, j - i + 1);
                 }
             }
         }
