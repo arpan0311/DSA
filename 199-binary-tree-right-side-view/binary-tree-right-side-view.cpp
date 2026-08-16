33e3dbda2dc3f7a4200a35fcf5137ac90fd2421a
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-17 03:07:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (14.72%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -6,36 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-                if (root == NULL) {
-                    return {};
+        if(!root){
+            return {};
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<int>answer;
+        while(!q.empty()){
+            int n=q.size();
+             vector<int>temp;
+            for(int i=0;i<n;i++){
+                TreeNode* root=q.front();
+                temp.push_back(root->val);
+                q.pop();
+                if(root->left){
+                    q.push(root->left);
                 }
-                queue<TreeNode*> q;
-                q.push(root);
-
-                vector<int> answer;
-                while (!q.empty()) {
-                    int size = q.size();
-                    vector<int> temp;
-                    for (int i = 0; i < size; i++) {
-                        TreeNode* curr = q.front();
-                        q.pop();
-                        if (curr->left != NULL) {
-                            q.push(curr->left);
-                        }
-                        if (curr->right != NULL) {
-                            q.push(curr->right);
-                        }
-                        temp.push_back(curr->val);
-                    }
-                    answer.push_back(temp.back());
+                if(root->right){
+                    q.push(root->right);
                 }
-                return answer;
             }
-        };
+            answer.push_back(temp.back()); 
+        }
+        return answer;
+    }
+};
