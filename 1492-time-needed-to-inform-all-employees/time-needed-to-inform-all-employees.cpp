ee55e4e8a5495da2948c6a436fabfdf2be236213
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-19 23:43:01 +0530

    Time: 492 ms (5.03%) | Memory: 236.4 MB (5.03%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-int max_time=0;
-    void dfs( unordered_map<int,vector<int>>&adj,vector<int>& informTime,int curr_id,int time){
-        max_time=max(max_time,time);
-
-        for(auto&it:adj[curr_id]){
-            dfs(adj,informTime,it,time+informTime[curr_id]);
-        }
-    }
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        unordered_map<int,vector<int>>adj;
-        // adj list manager to staff
-        for(int i=0;i<manager.size();i++){
-            // ye to sabka maneger hain isko kya need 
-            if(manager[i]==-1){
-                continue;
+    int bfs(int s, unordered_map<int, vector<int>> tree, int& maxi,
+            unordered_map<int, int>& taken_time, int curr_time) {
+        maxi = max(maxi, curr_time);
+        queue<pair<int, int>> q;
+        q.push({s, 0});
+        int min_time_taken = 0;
+        while (!q.empty()) {
+            int st = q.front().first;
+            int curr_taken_time = q.front().second;
+            q.pop();
+            min_time_taken = max(min_time_taken, curr_taken_time);
+            for (auto& it : tree[st]) {
+                q.push({it, curr_taken_time + taken_time[it]});
             }
-            adj[manager[i]].push_back(i);
         }
-        dfs(adj,informTime,headID,0);
-        return max_time;
+        return min_time_taken;
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager,
+                     vector<int>& informTime) {
+        unordered_map<int, vector<int>> tree;
+
+        unordered_map<int, int> taken_time;
+        for (int i = 0; i < manager.size(); i++) {
+            tree[manager[i]].push_back(i);
+        }
+
+        for (int i = 0; i < informTime.size(); i++) {
+            taken_time[i] = informTime[i];
+        }
+        // start dfs from tree[-1]
+        int maxi = 0;
+        return bfs(-1, tree, maxi, taken_time, 0);
     }
 };
