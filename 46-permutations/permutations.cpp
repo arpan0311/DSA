312aeac6d612d5a08666da8a91c0096f9ba332be
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-20 17:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (52.00%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<vector<int>>&answer,vector<int>temp,vector<bool>&pick){
-        if(temp.size()==nums.size()){
+    void solve(vector<int>& nums, vector<int>& temp,
+               vector<vector<int>>& answer, int mask) {
+        if (temp.size() == nums.size()) {
             answer.push_back(temp);
             return;
         }
 
-        for(int i=0;i<nums.size();i++){
-            if(!pick[i]){
-                pick[i]=true;
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (!(mask & (1 << i))) {
                 temp.push_back(nums[i]);
-                solve(nums,answer,temp,pick);
+
+                solve(nums, temp, answer, mask | (1 << i));
+
                 temp.pop_back();
-                pick[i]=false;
             }
         }
-
-
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>answer;
-        vector<int>temp;
-        vector<bool>pick(nums.size(),false);
-        solve(nums,answer,temp,pick);
+        int bits = 0;
+        vector<vector<int>> answer;
+
+        int n = nums.size();
+        vector<int> temp;
+        solve(nums, temp, answer, bits);
         return answer;
     }
 };
