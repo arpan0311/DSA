a27fafe6d9581eadc81c61efb49285eccc03fb13
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-08-24 00:06:52 +0530

    Time: 33 ms (36.22%) | Memory: 13 MB (85.60%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int dp[1001][1001];
-        memset(dp,0,sizeof(dp));
-        int n=text1.size(),m=text2.size();
-        // int n=text1.size(),m=text2.size();
-        for(int i=1;i<=text1.size();i++){
-            for(int j=1;j<=text2.size();j++){
-            
-                if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+int dp[1001][1001];
+    int solve(string &s1,string &s2,int i,int j){
+        if(i<0||j<0){
+            return 0;
         }
-        return dp[n][m];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int take=0,skip_i=0,skip_j=0;
+        if(s1[i]==s2[j]){
+            take=1+solve(s1,s2,i-1,j-1);
+        }
+        else{
+            skip_i=solve(s1,s2,i-1,j);
+            skip_j=solve(s1,s2,i,j-1);
+        }
+        return dp[i][j]= max(take,max(skip_i,skip_j));
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int i=text1.size()-1,j=text2.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(text1,text2,i,j);
     }
 };
