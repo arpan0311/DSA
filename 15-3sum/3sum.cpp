4bc40e1d7026cc2caa188168a468846902d3e92e
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-09-09 22:10:26 +0530

    Time: 47 ms (62.61%) | Memory: 29 MB (92.32%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,47 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i=0,j=i+1,n=nums.size();
-          vector<vector<int>>answer;
-        for(int i=0;i<nums.size();i++){
-            if(i>0&&nums[i-1]==nums[i]){
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> answer;
+        for (int i = 0; i < nums.size(); i++) {
+            int j = i + 1, k = nums.size() - 1;
+            // I already cover this element don't need to cover anymore
+            if (i - 1 >= 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int j=i+1,k=n-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum<0){
-                    j=j+1;
-                    while(j+1<k&&nums[j-1]==nums[j]){
+            int target = nums[i];
+            while (j < k) {
+                int curr_value = nums[j] + nums[k] + nums[i];
+                // I have to increse value
+                if (curr_value < 0) {
+                    j++;
+                    // decrese value
+                } else if (curr_value > 0) {
+                    k--;
+                } else {
+                    answer.push_back({nums[i], nums[j], nums[k]});
+                    while (j + 1 <= k && nums[j] == nums[j + 1]) {
                         j++;
                     }
-                }
-                else if(sum>0){
-                     k=k-1;
-                    while(k>j&&nums[k]==nums[k+1]){
+                    while (k - 1 >= j && nums[k] == nums[k - 1]) {
                         k--;
                     }
-                }
-                else{
-                    vector<int>temp;
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[j]);
-                    temp.push_back(nums[k]);
-                    answer.push_back(temp);
-                     j=j+1;
-                    while(j+1<k&&nums[j-1]==nums[j]){
-                        j++;
-                    }
-                     k=k-1;
-                    while(k>j&&nums[k]==nums[k+1]){
-                        k--;
-                    }
+                    j++;
+                    k--;
                 }
             }
-           
         }
         return answer;
     }
-
 };
