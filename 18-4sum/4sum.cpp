27a48d66d7ee096d0a0d6bb643cd5044b7903d04
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-09-10 12:04:44 +0530

    Time: 17 ms (85.41%) | Memory: 17.4 MB (57.75%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        if (nums.size() < 4) {
+            return {};
+        }
         sort(nums.begin(), nums.end());
-        int n = nums.size();
         vector<vector<int>> answer;
-
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue; 
-
-                int k = j + 1, l = n - 1;
-
+        for (int i = 0; i < nums.size() - 3; i++) {
+            if (i - 1 >= 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.size() - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int k = j + 1, l = nums.size() - 1;
                 while (k < l) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
-
-                    if (sum == target) {
-                        answer.push_back({nums[i], nums[j], nums[k], nums[l]});
-
-                        
-                        while (k < l && nums[k] == nums[k + 1]) k++;
-                       
-                        while (k < l && nums[l] == nums[l - 1]) l--;
-
+                    long long curr_sum =
+                        (long long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if (curr_sum < target) {
                         k++;
+                    } else if (curr_sum > target) {
                         l--;
-                    } 
-                    else if (sum < target) {
+                    } else {
+                        answer.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        while (k + 1 < l && nums[k] == nums[k + 1]) {
+                            k++;
+                        }
+                        while (l - 1 > k && nums[l] == nums[l - 1]) {
+                            l--;
+                        }
                         k++;
-                    } 
-                    else {
                         l--;
                     }
                 }
