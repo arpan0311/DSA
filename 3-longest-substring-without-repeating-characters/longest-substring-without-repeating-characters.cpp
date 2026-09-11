df7216a4e51e0e3cd29162b47cc18cf62f402be1
arpan0311 <156085825+arpan0311@users.noreply.github.com>
2026-09-11 16:19:53 +0530

    Time: 7 ms (81.75%) | Memory: 19.2 MB (45.08%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-       unordered_map<int,int>hash;
-       int i=0,j=0;
-       int answer=0;
-       while(j<s.length()){
-        hash[s[j]]++;
-        while(hash[s[j]]>1){
-            hash[s[i]]--;
-            i++;
+        vector<int> can_do(256, -1);
+        int i = 0;
+        int max_len = 0;
+        for (int j = 0; j < s.size(); j++) {
+            // means phale se hai
+            if (can_do[s[j]] != -1) {
+                if (can_do[s[j]] >= i) {
+                    i = can_do[s[j]] + 1;
+                }
+            }
+            int curr_len = j - i + 1;
+            max_len = max(max_len, curr_len);
+            can_do[s[j]]=j;
         }
-        answer=max(answer,j-i+1);
-        j++;
-       } 
-       return answer;
+        return max_len;
     }
 };
